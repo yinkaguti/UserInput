480a9a46a0aec403ddc0a45f7394c001d583908f
yinkaguti <olayinka2good@yahoo.co.uk>
2019-05-17 09:20:48 +0100

    coding

---
+++ UserInput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class UserInput{
+    public static void main(String[] args);
+
+    Scanner sc=new Scanner (System.in);
+    System.out.print("please enter your age");
+    int age= in.nextln();
+    
+    System.out.println("");
+}
